dce8e26cd2ad3ba37e385289841e58a249c398bb
-help <somepost@mail.com>
2021-02-25 16:23:15 +0200

    Add NavBar and Login page layoult

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,12 +1,11 @@
 import express from 'express';
 const router = express.Router();
 import * as homeController from '../controllers/home.controller'
+
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('pages/index', { title: 'Express' });
-// });
-
-
-router.get('/', homeController.index)
+router.get('/', homeController.index);
+router.get('/login', function (req,res,nex) {
+    res.render('pages/login', {title: 'UserList Login'});
+});
 
 export default router;
